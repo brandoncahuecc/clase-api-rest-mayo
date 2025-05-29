b8aecb7daa56280b8bd3df0ca09f15d43c1812bd
brandoncahuecc <brandoncahuecc@outlook.com>
2025-05-28 21:09:39 -0600

    Clase 01: Practica crud clientes

---
+++ clase-uno/Controllers/ClientesController.cs
@@ -0,0 +1,44 @@
+﻿using clase_uno.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clase_uno.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Cliente> _clientes = new List<Cliente>();
+
+        [HttpGet]
+        public IActionResult GetCliente()
+        {
+            return Ok(_clientes);
+        }
+
+        [HttpPost]
+        public IActionResult PostCliente(Cliente cliente)
+        {
+            int id = _clientes is null || _clientes.Count == 0
+                ? 0 : _clientes.Max(c => c.Id);
+
+            cliente.Id = id + 1;
+            _clientes.Add(cliente);
+
+            return Ok(cliente);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCliente(int id)
+        {
+            Cliente? cliente = _clientes.FirstOrDefault(c => c.Id == id);
+
+            if (cliente is null)
+                return NotFound();
+
+            _clientes.Remove(cliente);
+
+            return Ok();
+        }
+    }
+}
---
+++ clase-uno/Modelos/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace clase_uno.Modelos
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateOnly FechaNacimiento { get; set; }
+        public int Celular { get; set; }
+    }
+}
